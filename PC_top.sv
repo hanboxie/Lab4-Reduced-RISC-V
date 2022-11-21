0cb136421e97464d3295189a95ed45434c5a09c0
Karry939 <116262630+Karry939@users.noreply.github.com>
2022-11-21 20:36:32 +0000

    new updates

---
+++ PC_top.sv
@@ -0,0 +1,29 @@
+module PC_top #(
+    parameter WIDTH = 32;
+
+)(
+    input logic clk,
+    input logic rst,
+    input logic [WIDTH-1:0] ImmOp,
+    output logic [WIDTH-1:0] PC
+);
+
+ logic [WIDth-1:0]  inc_PC;
+ logic [WIDTH-1:0]  branch_PC;
+ assign inc_PC = PC+4;
+ assign branch_PC = PC + ImmOp;
+
+
+  mux1 Mux (
+    .PCsrc  (PCsrc),
+    .inc_PC (inc_PC),
+    .branch_PC (branch_PC),
+    .next_PC (next_PC)
+  );
+
+  ProgramCounter ProgramCounter (
+    .clk (clk),
+    .rst (rst),
+    .next_PC (next_PC),
+    .ProgramCounter(ProgramCounter)
+  );
